ad9e6b4961c9aa1a588fe9df17386f853ea38d21
Ruslan <b-r1@yandex.ru>
2020-07-19 02:15:09 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,26 @@ $(function() {
         slidesToShow: 4,
         slidesToScroll: 4
     });
+    $("#example_id").ionRangeSlider({
+        type: "double",
+        grid: false,
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function() {
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
+    $('.icon-th-large').on('click', function() {
+        $('.product__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+    });
 
     var mixer = mixitup('.products__inner-box');
 
